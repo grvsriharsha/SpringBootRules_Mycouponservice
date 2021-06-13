0a1360a6b95779a5e0943d26d8f7b95f9861bbbc
grvsriharsha <58652321+grvsriharsha@users.noreply.github.com>
2021-06-13 16:53:57 +0530

    Update tables.sql
    Making it suitable for mssql server

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,14 +1,14 @@
 use mydb;
 
 create table product(
-id int AUTO_INCREMENT PRIMARY KEY,
+id int IDENTITY(1,1) NOT NULL,
 name varchar(20),
 description varchar(100),
 price decimal(8,3) 
 );
 
 create table coupon(
-id int AUTO_INCREMENT PRIMARY KEY,
+id int IDENTITY(1,1) NOT NULL,
 code varchar(20) UNIQUE,
 discount decimal(8,3),
 exp_date varchar(100) 
